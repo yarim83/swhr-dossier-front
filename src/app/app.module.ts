1112775a294087c78a6c61e9ebe3194c5014e03d
Tomasz Smereczynski <tomek.smereczynski@gmail.com>
2020-03-15 20:16:24 +0100

    Implementacja tworzenia, detali, listy i aktualizacji.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app.routing.module';
 import { AppComponent } from './app.component';
 import { CreateEmployeeComponent } from './create-employee/create-employee.component';
@@ -23,6 +23,7 @@ import { ConfirmationPopoverModule} from 'angular-confirmation-popover';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
     ConfirmationPopoverModule.forRoot({
       confirmButtonType: 'danger'
     })
--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -2,6 +2,9 @@ import {EmployeeService} from '../employee.service';
 import {Employee} from '../models/employee';
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {FormGroup} from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
+import {Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,15 +13,27 @@ import {Router} from '@angular/router';
 })
 export class CreateEmployeeComponent implements OnInit {
 
+  registerForm: FormGroup;
   employee: Employee = new Employee();
+  dupa: Employee = new Employee();
   submitted = false;
+
   sexs = ['kobieta', 'meżczyzna'];
 
   constructor(private employeeService: EmployeeService,
-              private router: Router) {
+              private router: Router,
+              private formBuilder: FormBuilder) {
   }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      pesel: ['', Validators.required],
+      sex: ['', Validators.required],
+      dateOfBirth: ['', Validators.required]
+    });
   }
 
   newEmployee(): void {
@@ -27,15 +42,39 @@ export class CreateEmployeeComponent implements OnInit {
   }
 
   save() {
-    this.employeeService.createEmployee(this.employee)
+    this.dupa.firstName  = this.registerForm.value.firstName;
+    this.dupa.lastname = this.registerForm.value.lastName;
+    this.dupa.email = this.registerForm.value.email;
+    this.dupa.pesel = this.registerForm.value.pesel;
+    this.dupa.sex = this.registerForm.value.sex;
+    this.dupa.birth_date = this.registerForm.value.dateOfBirth;
+
+    console.log(this.dupa);
+    this.employeeService.createEmployee(this.dupa)
       .subscribe(data => console.log(data), error => console.log(error));
     this.employee = new Employee();
     this.gotoList();
   }
 
+  get f() {
+    return this.registerForm.controls;
+  }
+
   onSubmit() {
     this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.registerForm.value, null, 4));
+
     this.save();
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.registerForm.reset();
   }
 
   gotoList() {
